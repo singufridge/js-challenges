ce6ce858105f7366ff3eb575b3092e9f054f10ec
spencer-m8 <shsmclean@gmail.com>
2026-05-21 16:04:47 -0400

    keydown and click add to local storage

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 let flag = false;
+let eventNum = 0;
 
 document.addEventListener('keydown', (event) => {
     console.log(event);
+    //keydownIdNum = keyCode + timestamp
+    let keyVal = Math.round(event.keyCode + event.timeStamp);
+    window.localStorage.setItem(eventNum, keyVal);
+    eventNum++;
 });
 
 document.addEventListener('click', (event) => {
+
     console.log(event);
-    window.localStorage.setItem("pointerId", event.pointerId)
+    //pointerVal = screenX + screenY + timestamp
+    let pointerVal = Math.round(event.screenX + event.screenY + event.timeStamp);
+    window.localStorage.setItem(eventNum, pointerVal);
+    eventNum++;
 });
