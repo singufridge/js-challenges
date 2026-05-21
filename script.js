297612d6fd591fe696dc3aff19ff33295c24faec
singufridge <cadence.delia382@gmail.com>
2026-05-21 16:08:50 -0400

    generate tags based on value

--- script.js
+++ script.js
@@ -7,4 +7,23 @@ document.addEventListener('keydown', (event) => {
 document.addEventListener('click', (event) => {
     console.log(event);
     window.localStorage.setItem("pointerId", event.pointerId)
+    updateChanges();
 });
+
+
+function updateChanges() {
+    for (i = 0; i < localStorage.length; i++) {
+        if (localStorage.getItem(localStorage.key(i)) > 0) {
+            console.log("create p")
+
+            const p = document.createElement('p');
+            p.textContent = localStorage.getItem(localStorage.key(i));
+            document.body.appendChild(p);
+
+        } else if (localStorage.getItem(localStorage.key(i)) % 3 === 0) { 
+            console.log("divisiblwe by 3")
+        } else {
+            console.log(":(")
+        }
+    }
+}
